d30f491c38d588dc5aaefd13178624b8412063aa
Amey Kunte <amey.k@research.iiit.ac.in>
2023-04-13 07:39:43 +0530

    updated the code,extended some functionality
    This code is a Bash script that automates the installation of important applications on a fresh Ubuntu installation. It includes a function to install snap packages for applications such as Discord, Notion, and Spotify, as well as basic tools like pip and npm. The script also updates the system and cleans up unnecessary packages.

--- initialiser.sh
+++ initialiser.sh
@@ -1,21 +1,87 @@
-#! /bin/bash
-echo "Dowloading the important apps,please wait :)"
-echo $(sudo snap install discord)
-echo $(sudo snap install notion-snap)
-echo $(sudo snap install spotify)
-echo $(sudo snap install whatsapp-for-linux)
-echo $(sudo snap install teams)
-echo $(sudo snap install vlc)
-echo $(sudo snap install prospect-mail)
-echo $(sudo snap install code --classic)
-echo "downloaded the apps,updating the system"
-echo $(sudo apt-get update && sudo apt-get upgrade -y)
-echo $(sudo apt-get install preload)
-echo $(sudo apt-get install git)
+#!/bin/bash
 
-echo "updated the system,finishing final changes"
-echo $(sudo apt-get autoremove)
-echo $(sudo apt-get autoclean)
-echo "All done,enjoy your new laptop"
+# Function to install snap packages
+function install_snap_packages {
+    echo "Installing snap packages..."
+    read -p "Install Discord? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install discord
+    fi
 
-exit
+    read -p "Install Notion? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install notion-snap
+    fi
+
+    read -p "Install Spotify? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install spotify
+    fi
+
+    read -p "Install WhatsApp for Linux? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install whatsapp-for-linux
+    fi
+
+    read -p "Install Teams? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install teams
+    fi
+
+    read -p "Install VLC? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install vlc
+    fi
+
+    read -p "Install Prospect Mail? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install prospect-mail
+    fi
+
+    read -p "Install VS Code? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo snap install code --classic
+    fi
+}
+
+# Function to install basic tools
+function install_basic_tools {
+    echo "Installing basic tools..."
+    sudo apt-get install -y git curl wget build-essential
+    sudo apt-get install -y python3-pip
+    sudo apt-get install -y nodejs npm
+}
+
+# Function to update the system
+function update_system {
+    echo "Updating the system..."
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install -y preload
+}
+
+# Function to clean up the system
+function clean_up {
+    echo "Cleaning up the system..."
+    sudo apt-get autoremove -y
+    sudo apt-get autoclean -y
+}
+
+# Main function
+function main {
+    echo "Downloading and installing important apps..."
+    install_snap_packages
+    install_basic_tools
+    update_system
+    clean_up
+    echo "All done! Enjoy your new laptop."
+}
+
+main
