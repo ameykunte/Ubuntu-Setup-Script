48dfe57a2e10f9b296949bc820703372aac60af8
Amey Kunte <amey.k@research.iiit.ac.in>
2023-05-06 12:57:43 +0530

    Update initialiser.sh with progress bar

--- initialiser.sh
+++ initialiser.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Function to install snap packages
 function install_snap_packages {
@@ -6,75 +7,78 @@ function install_snap_packages {
     read -p "Install Discord? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install discord
+        sudo snap install discord | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Notion? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install notion-snap
+        sudo snap install notion-snap | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Spotify? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install spotify
+        sudo snap install spotify | pv -p -t -e >/dev/null
     fi
 
     read -p "Install WhatsApp for Linux? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install whatsapp-for-linux
+        sudo snap install whatsapp-for-linux | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Teams? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install teams
+        sudo snap install teams | pv -p -t -e >/dev/null
     fi
 
     read -p "Install VLC? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install vlc
+        sudo snap install vlc | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Prospect Mail? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install prospect-mail
+        sudo snap install prospect-mail | pv -p -t -e >/dev/null
     fi
 
     read -p "Install VS Code? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install code --classic
+        sudo snap install code --classic | pv -p -t -e >/dev/null
     fi
 }
 
 # Function to install basic tools
 function install_basic_tools {
     echo "Installing basic tools..."
-    sudo apt-get install -y git curl wget build-essential
-    sudo apt-get install -y python3-pip
-    sudo apt-get install -y nodejs npm
+    sudo apt-get install -y git curl wget build-essential | pv -p -t -e >/dev/null
+    sudo apt-get install -y python3-pip | pv -p -t -e >/dev/null
+    sudo apt-get install -y nodejs npm | pv -p -t -e >/dev/null
 }
 
 # Function to update the system
 function update_system {
     echo "Updating the system..."
-    sudo apt-get update && sudo apt-get upgrade -y
-    sudo apt-get install -y preload
-    sudo apt-get install -y htop 
-    sudo apt-get install tlp tlp-rdw
-
+    sudo apt-get update | pv -p -t -e >/dev/null
+    sudo apt-get upgrade -y | pv -p -t -e >/dev/null
+    sudo apt-get install -y preload | pv -p -t -e >/dev/null
+    sudo apt-get install -y htop | pv -p -t -e >/dev/null
+    sudo apt-get install tlp tlp-rdw | pv -p -t -e >/dev/null
 }
+
+# Function to clean up the system
+
 
 # Function to clean up the system
 function clean_up {
     echo "Cleaning up the system..."
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean -y
+    sudo apt-get autoremove -y | pv -p -t -e >/dev/null
+    sudo apt-get autoclean -y | pv -p -t -e >/dev/null
 }
 
 # Main function
